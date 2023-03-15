e2873daef88526e2ca97ddcbc6819a88a4d13274
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-03-15 12:15:33 +0900

    contextで、propsのバケツリレーを排除した。

--- global-state/src/App.jsx
+++ global-state/src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useContext } from "react";
+import { AdminFlagContext } from "./components/providers/AdminFlagProvider";
 import { Card } from "./components/Card";
 
 export const App = () => {
     //管理者フラグ
-    const [isAdmin, setIsAdmin] = useState(false);
+    const {isAdmin, setIsAdmin} = useContext(AdminFlagContext);
 
     //切り替え押下時
     const onClickSwitch = () => setIsAdmin(!isAdmin);
@@ -12,7 +13,7 @@ export const App = () => {
         <div>
             {isAdmin ? <span>管理者です</span> : <span>管理者以外です</span>}
             <button onClick={onClickSwitch}>切り替え</button>
-            <Card isAdmin={isAdmin} />
+            <Card />
         </div>
     )
 }
--- global-state/src/components/providers/AdminFlagProvider.jsx
+++ global-state/src/components/providers/AdminFlagProvider.jsx
@@ -1,15 +1,15 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AdminFlagContext = createContext({});
 
 export const AdminFlagProvider = props => {
     const { children } = props;
 
-    const sampleObj = { sampleValue: "テスト" };
+    const [isAdmin, setIsAdmin] = useState(false);
 
     //valueの中に、グローバルに扱う実際の値を設定
     return (
-        <AdminFlagContext.Provider value={sampleObj}>
+        <AdminFlagContext.Provider value={{isAdmin, setIsAdmin}}>
             {children}
         </AdminFlagContext.Provider>
     );
