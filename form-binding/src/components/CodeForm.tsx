8f1a9b76218d2cbb300b7cfa4314762c9c8e2b09
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-03-18 22:13:39 +0900

    フォームを増やして、テキストをいじれるようにした。

--- form-binding/src/components/CodeForm.tsx
+++ form-binding/src/components/CodeForm.tsx
@@ -1,5 +1,14 @@
-export const CodeForm = () => {
+import { useState } from "react"
+
+type formType = {
+    formStr: string
+}
+
+export const CodeForm = (props: formType) => {
+
+    const [formStr, setFormStr] = useState(props.formStr);
+
     return (
-        <textarea value="こんにちは"></textarea>
+        <textarea value={formStr} onChange={(event) => setFormStr(event.target.value)}></textarea>
     )
 }
--- form-binding/src/components/Form.tsx
+++ form-binding/src/components/Form.tsx
@@ -5,10 +5,18 @@ import { CodeForm } from "./CodeForm";
 export const Form = () => {
     const [code, setCode] = useState<string[]>([]);
 
+    const addForm = () => {
+        const str: string = "テスト";
+        setCode([...code,str]);
+    }
+
     return (
         <div>
             <h1>コンポーネント</h1>
-            <CodeForm />
+            <button onClick={addForm}>追加</button>
+            {code.map(codeFormText => (
+                <CodeForm formStr={codeFormText} />
+            ))}
         </div>
     );
 };
